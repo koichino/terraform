aeb0b2570e6769b789665f9293604f26bd2b3eda
koichino <koichino@microsoft.com>
2025-08-08 17:15:47 +0900

    Add azapi privider

--- koichino_wk/aoai/providers.tf
+++ koichino_wk/aoai/providers.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    azapi = {
+      source = "Azure/azapi"
+      version = "~>2.0"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>4.0"
@@ -9,6 +13,11 @@ terraform {
       version = "~>3.0"
     }
   }
+}
+
+
+provider "azapi" {
+  subscription_id = "44507848-c16e-4ccd-b18c-487609de3470"
 }
 
 provider "azurerm" {
